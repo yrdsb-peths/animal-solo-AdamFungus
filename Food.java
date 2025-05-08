ffbae033a0160f08263d5f5c2c9051d5a4ae4fe8
AdamFungus <afsomerset@gmail.com>
2025-05-08 09:48:14 -0400

    Death Screen

--- Food.java
+++ Food.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Food extends Actor
 {
+    int Y = 0;
     /**
      * Act - do whatever the Food wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,6 +16,11 @@ public class Food extends Actor
     public void act()
     {
         setLocation(getX(), getY() + 1);
-        
+        Y = getY();
+        if(Y >= 300) {
+            Dead dead = new Dead();
+            getWorld().addObject(dead, 300, 200);
+            getWorld().removeObject(this);
+        }
     }
 }
